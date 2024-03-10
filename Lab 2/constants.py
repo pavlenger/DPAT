7c0421ff169416ef15c4e320fbfbad779e23b8c3
pavlenger <venger.pavlo@gmail.com>
2024-03-10 21:42:38 +0200

    Edit Jupyter Notebook

--- Lab 2/constants.py
+++
@@ -1,5 +0,0 @@
-DIRECTORY = 'CSV_Files/'
-URL = ('https://www.star.nesdis.noaa.gov/smcd/emb/vci/VH/get_TS_admin.php?country=UKR&provinceID={'
-       '}&year1=1981&year2=2020&type=Mean')
-INDEXES = {1: 22, 2: 24, 3: 23, 4: 25, 5: 3, 6: 4, 7: 8, 8: 19, 9: 20, 10: 21, 11: 9, 13: 10, 14: 11, 15: 12, 16: 13,
-           17: 14, 18: 15, 19: 16, 21: 17, 22: 18, 24: 1, 25: 2, 26: 7, 27: 5}
